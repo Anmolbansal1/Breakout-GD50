6562e08cb80da83ff29065f9bc32974c5908ad1d
Anmol Bansal <banmol0001@gmail.com>
2018-07-28 12:32:12 +0530

    Breakout5
    Added lives, score and game over state.

--- main.lua
+++ main.lua
@@ -51,7 +51,8 @@ function love.load()
     gFrames = {
         ['paddles'] = GenerateQuadsPaddles(gTextures['main']),
         ['balls']   = GenerateQuadsBalls(gTextures['main']),
-        ['bricks']  = GenerateQuadsBricks(gTextures['main']) 
+        ['bricks']  = GenerateQuadsBricks(gTextures['main']) ,
+        ['hearts']  = GenerateQuads(gTextures['hearts'], 10, 9)
     }
 
     push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
@@ -91,7 +92,9 @@ function love.load()
     -- 6. 'game-over' (the player has lost; display score and allow restart)
     gStateMachine = StateMachine {
         ['start'] = function() return StartState() end,
-        ['play']  = function() return PlayState() end
+        ['play']  = function() return PlayState() end,
+        ['serve'] = function() return ServeState() end,
+        ['game-over'] = function() return GameOverState() end
     }
 
     gStateMachine:change('start')
@@ -145,4 +148,26 @@ function displayFPS()
     love.graphics.setFont(gFonts['small'])
     love.graphics.setColor(0, 255, 0, 255)
     love.graphics.print('FPS: '.. tostring(love.timer.getFPS()), 5, 5)
+end
+
+function renderHealth(health)
+    
+    local healthX = VIRTUAL_WIDTH - 100
+
+    for i = 1, health do
+        love.graphics.draw(gTextures['hearts'], gFrames['hearts'][1], healthX, 4)
+        healthX = healthX + 11
+    end
+
+    for i = 1, 3 - health do
+        love.graphics.draw(gTextures['hearts'], gFrames['hearts'][2], healthX, 4)
+        healthX = healthX + 11
+    end
+end
+
+
+function renderScore(score)
+    love.graphics.setFont(gFonts['small'])
+    love.graphics.print('Score: ', VIRTUAL_WIDTH - 60, 5)
+    love.graphics.printf(tostring(score), VIRTUAL_WIDTH - 50, 5, 40, 'right')
 end
